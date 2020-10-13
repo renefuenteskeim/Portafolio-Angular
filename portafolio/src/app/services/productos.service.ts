f25054517b653f8344219cea9f7dde2ef2f3995c
Rene <rene.fuentes@alumnos.uach.cl>
2020-10-13 18:36:09 -0300

    search funcionando y descripcion productos

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -1,27 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Producto} from '../interfaces/producto.interface';
+import { rejects } from 'assert';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
   productos : Producto [] = [];
+  productoFiltrado : Producto [] = [];
   constructor(private http:HttpClient) { 
     this.cargarProductos();
   }
   
   private cargarProductos() {
-    this.http.get('https://angular-html-portafolio-249c2.firebaseio.com/productos_idx.json')
+
+    return new Promise ((resolve,reject) =>{
+
+      this.http.get('https://angular-html-portafolio-249c2.firebaseio.com/productos_idx.json')
     .subscribe((resp : Producto[])=>{
-      console.log(resp);
       this.productos=resp;
+      this.cargando=false;
+      resolve();
+    });
+    });
 
-      setTimeout(()=>{
-        this.cargando=false;
-      },2000);
+  }
 
-      
+  getProducto(id:string){
+    return this.http.get(`https://angular-html-portafolio-249c2.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino:string){
+
+    if(this.productos.length === 0 ){
+      this.cargarProductos().then(()=>{
+        this.filtrarProductos( termino);
+      });
+    }else{
+      this.filtrarProductos( termino);
+    }
+  }
+  private filtrarProductos(termino:string){
+    //console.log(this.productos);
+    this.productoFiltrado =[];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf( termino ) >=0 || tituloLower.indexOf(termino) >=0){
+        this.productoFiltrado.push(prod);
+      }
     });
   }
 }
