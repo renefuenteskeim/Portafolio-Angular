c57678ea091158d51f7fc9a935af16e6eaa3bde6
Rene <rene.fuentes@alumnos.uach.cl>
2020-10-13 03:18:07 -0300

    services y conexion a firebase

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -9,13 +9,29 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo : any [] =[];
   constructor( private http: HttpClient) {
     //console.log("Servicio cargado");
 
+    this.cargarInfo();
+    this.cargaEquipo();
+    
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) =>{
         this.cargada = true;
         this.info = resp;
       });
   }
+
+  private cargaEquipo (){
+    this.http.get('https://angular-html-portafolio-249c2.firebaseio.com/equipo.json')
+      .subscribe( (resp: any []) =>{
+        this.equipo= resp;
+        console.log(this.equipo);
+      });
+  }
+
 }
